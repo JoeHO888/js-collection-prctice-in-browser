84f1c977603cfd566e9acd5ac7f18742d2df8f28
JOE HO (INFRA-ISD-OOCLL/HKG) <HOJO3@OOCL.COM>
2018-11-07 12:45:02 +0800

    Revert "section1_pratice_1"
    This reverts commit 61345a9736b3a74af108489a9eabfba90813a559.

--- practice/section-1/practice-1-spec.js
+++ practice/section-1/practice-1-spec.js
@@ -13,22 +13,3 @@ describe('practice-1-1', () => {
     expect(result).toEqual(['a', 'e', 'f', 'd']);
   });
 });
-
-
-function collectSameElements(collectionA, collectionB) {
-  var c = new Array();
-  for(var i = 0; i<collectionA.length ; i++)
-  {
-    for(var j = 0; j<collectionB.length ; j++)
-    {
-      if (collectionA[i] == collectionB[j])
-      {
-        c.push(collectionA[i]);
-        break;
-      }
-    }
-  }
-  return c;
-
-}
-
