4b09f04f3104f65f950b2443c7f3256d61257b94
JOE HO (INFRA-ISD-OOCLL/HKG) <HOJO3@OOCL.COM>
2018-11-07 13:05:45 +0800

    section_1_pratice_2

--- main/section-1/practice-2-spec.js
+++ main/section-1/practice-2-spec.js
@@ -13,3 +13,22 @@ describe('practice-1-2', () => {
     expect(result).toEqual(['a', 'e', 'f', 'd']);
   });
 });
+
+
+function collectSameElements(collectionA, collectionB) {
+  var c = new Array();
+  collectionB = collectionB[0]
+  for(var i = 0; i<collectionA.length ; i++)
+  {
+    for(var j = 0; j<collectionB.length ; j++)
+    {
+      if (collectionA[i] == collectionB[j])
+      {
+        c.push(collectionA[i]);
+        break;
+      }
+    }
+  }
+  return c;
+
+}
