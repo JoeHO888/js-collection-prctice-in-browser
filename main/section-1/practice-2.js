323b3d8c3ae581f1a3752c4f8c663d1951dba46a
HOJO3 <joe.ho@oocl.com>
2018-11-21 12:04:50 +0800

    file update

--- main/section-1/practice-2.js
+++ main/section-1/practice-2.js
@@ -3,3 +3,6 @@
 function collectSameElements(collectionA, collectionB) {
   return 'Implement the practice require, and begin changing code in this row';
 }
+
+
+const collectSameElements = (collectionA, collectionB)=>collectionA.filter(a=>collectionB[0].includes(a))
--- main/section-1/practice-3.js
+++ main/section-1/practice-3.js
@@ -1,5 +1,5 @@
 'use strict';
 
-function collectSameElements(collectionA, objectB) {
-  return 'Implement the practice require, and begin changing code in this row';
-}
+
+
+const collectSameElements = (collectionA, collectionB)=>collectionA.filter(a=>collectionB.value.includes(a))
